4693cd6624804c85626296cf2720fdfb41d1e369
JaQwae <jaqwaeellison@gmail.com>
2025-02-16 12:05:16 -0800

    video added to home page as well as hero content positioned

--- client/src/components/Pages/home/Home.jsx
+++ client/src/components/Pages/home/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Home.css'
+import heroVideo from '../../../assets/home/placeholder-hero.mp4'
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div id='home-page' className='pages'>
+      <section id="hero-section" className='page-sections'>
+        <video autoPlay loop muted='true' id='homepage-hero-video'>
+          <source src={heroVideo} type="video/mp4"/>
+        </video>
+        <div className="overlay-screen">
+          <div id='homepage-hero-content'>
+            <h1 id="slogan"> &quot;Short Term Travel... Redefined.&quot;</h1>
+            <button id="cta-hero-btn" className='btn'><span>Book Your Journey</span></button>
+          </div>
+          <i className="fa-solid fa-angle-down"></i>
+        </div>
+      </section>
+    </div>
   )
 }
 
