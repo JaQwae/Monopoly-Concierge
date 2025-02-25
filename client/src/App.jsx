2a8c088bf96e0b2f3f7322b099768620c7c5ba8f
JaQwae <jaqwaeellison@gmail.com>
2025-02-24 18:40:28 -0800

    Responsively designed loading  screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
-import NavBar from './components/Navigation/NavBar'
+// import NavBar from './components/Navigation/NavBar'
+import Loading from './components/Loading/Loading'
 
 function App() {
   return (
-    <NavBar/>
+    // <NavBar/>
+    <Loading/>
   )
 }
 
