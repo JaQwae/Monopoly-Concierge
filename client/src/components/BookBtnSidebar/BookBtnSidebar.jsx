c39c9583d538d3840b8160e8f08d40772fedb487
JaQwae <jaqwaeellison@gmail.com>
2025-03-10 01:50:09 -0700

    added the html struture and general stylings to sidebar

--- client/src/components/BookBtnSidebar/BookBtnSidebar.jsx
+++ client/src/components/BookBtnSidebar/BookBtnSidebar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Drawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import PropTypes from 'prop-types';
+import CloseIcon from '@mui/icons-material/Close';
+import './BookBtnSidebar.css'
 
 const BookBtnSidebar = ({ isOpen, closeSidebar }) => {
   return (
@@ -11,13 +13,33 @@ const BookBtnSidebar = ({ isOpen, closeSidebar }) => {
       onClose={closeSidebar}
     >
       <Box
-        sx={{ width: 500 }}
-        role="presentation"
+        sx={{ width: 600 }}
+        aria-hidden="true"
         onClick={closeSidebar}
         onKeyDown={closeSidebar}
+        id='book-btn-services-tab'
       >
+        <button onClick={closeSidebar}><CloseIcon id='sidebar-x-icon' /></button>
+        <h3>Change This To Question</h3>
 
-        <button onClick={closeSidebar}>Close</button>
+        <section id='book-btn-services-container'>
+          <div className='book-btn-services'>
+            <img src="https://th.bing.com/th/id/OIP.6biXsVHLMu8Zd7GHhviDRQHaCx?rs=1&pid=ImgDetMain" alt='property image placeholder' />
+            <h4>Properties</h4>
+          </div>
+          <div className='book-btn-services'>
+            <img src="https://th.bing.com/th/id/OIP.Jh8daKibT-xEv_VtiQgVuQHaFj?rs=1&pid=ImgDetMain" alt='jet charter image placeholder' />
+            <h4>Jet Charters</h4>
+          </div>
+          <div className='book-btn-services'>
+            <img src="https://static1.hotcarsimages.com/wordpress/wp-content/uploads/2022/11/2pic-7.jpg" alt='car rental image placeholder' />
+            <h4>Car Rentals</h4>
+          </div>
+          <div className='book-btn-services'>
+            <img src="https://images.unsplash.com/photo-1445019980597-93fa8acb246c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt='concierge services image placeholder' />
+            <h4>Concierge Services</h4>
+          </div>
+        </section>
       </Box>
     </Drawer>
   );
