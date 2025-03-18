ccf8345e27747d3bc8c8fbf543b9c896e99e9724
JaQwae <jaqwaeellison@gmail.com>
2025-03-17 18:12:00 -0700

    made the code more readable in the prive.jsx file, state for the intro screen is updated there

--- client/src/components/Forms/prive-intro/PriveIntro.jsx
+++ client/src/components/Forms/prive-intro/PriveIntro.jsx
@@ -1,68 +1,33 @@
-import React, { useState } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import Button from '../../Buttons/Button'
-import { TextField } from '@mui/material'
-import './ProveIntro.css'
+import './PriveIntro.css'
 
-const PriveIntro = () => {
-    const [showForm, setShowForm] = useState(false);
+const PriveIntro = ({priveIntroVisible}) => {
 
     const handleContinueClick = (event) => {
         event.preventDefault();
-        setShowForm(true);
+        priveIntroVisible(false)
     };
 
+
     return (
-        <div id='prive-intro-container'>
-        {showForm ? (
-            <form>
-                <TextField 
-                    id="name-input" 
-                    label="Name" 
-                    type="text" 
-                    fullWidth 
-                    margin="normal" 
-                    sx={{
-                        backgroundColor: "black",
-                        input: { color: "white" }, // Text color
-                        "& .MuiOutlinedInput-root": {
-                          "& fieldset": { borderColor: "white" },
-                          "&:hover fieldset": { borderColor: "gray" },
-                          "&.Mui-focused fieldset": { borderColor: "white" },
-                        },
-                        "& .MuiInputLabel-root": { color: "white" }, // Label color
-                        "& .MuiInputLabel-root.Mui-focused": { color: "white" },
-                        "& .MuiInputBase-input::placeholder": { color: "rgba(255, 255, 255, 0.7)" }, // Placeholder color
-                      }}
-                />
-                <TextField 
-                    id="email-input" 
-                    label="Email" 
-                    type="email" 
-                    fullWidth 
-                    margin="normal" 
-                />
-                <TextField 
-                    id="phone-input" 
-                    label="Phone" 
-                    type="tel" 
-                    fullWidth 
-                    margin="normal" 
-                />
-                <Button btnIdName='submit-btn' displayName='SUBMIT' />
-            </form>
-        ) : (
-            <>
-                <p>
-                    Through our Privé Partners, we provide privileged access to luxury hotels, boutique
-                    accommodations, and exclusive stays. Our trusted partnerships ensure unparalleled hospitality
-                    and seamless reservations at some of the most prestigious properties. Simply choose your
-                    destination, and let us redefine your travel experience.
-                </p>
-                <Button btnIdName='prive-intro-btn' displayName='CONTINUE' btnAction={handleContinueClick} />
-            </>
-        )}
-    </div>
+        <div id='prive-intro-container' className='form-content-container'>
+            <p>
+                Through our Privé Partners, we provide privileged access to luxury hotels, boutique
+                accommodations, and exclusive stays. Our trusted partnerships ensure unparalleled hospitality
+                and seamless reservations at some of the most prestigious properties. Simply choose your
+                destination, and let us redefine your travel experience.
+            </p>
+            <Button btnIdName='prive-intro-btn' displayName='CONTINUE' btnAction={handleContinueClick} />
+        </div>
     )
 }
+
+
+PriveIntro.propTypes = {
+    priveIntroVisible: PropTypes.func.isRequired,
+}
+
 
 export default PriveIntro
--- client/src/components/Forms/single-form-content/SingleFormContent.jsx
+++ client/src/components/Forms/single-form-content/SingleFormContent.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import PriveIntro from '../prive-intro/PriveIntro'
 import { TextField } from '@mui/material'
+import './SingleFormContent.css'
 
 const SingleFormContent = ({ pageForm }) => {
     const [isPriveFrom, setIsPriveFrom] = useState(false);
@@ -9,22 +10,33 @@ const SingleFormContent = ({ pageForm }) => {
     useEffect(() => {
         if (pageForm === 'properties') {
             setIsPriveFrom(true)
-            console.log(isPriveFrom)
         }
-    }, [pageForm, isPriveFrom])
+    }, [pageForm])
 
 
 
     return (
         <>
-            {isPriveFrom ? <PriveIntro /> :
-                <section>
+            {isPriveFrom ? <PriveIntro priveIntroVisible={setIsPriveFrom} /> :
+                <section className='form-content-container'>
                     <p>{pageForm}</p>
                     <TextField
                         id="password-input"
                         label="Password"
                         type="password"
                         autoComplete="current-password"
+                        sx={{
+                            backgroundColor: "black",
+                            input: { color: "white" }, // Text color
+                            "& .MuiOutlinedInput-root": {
+                                "& fieldset": { borderColor: "white" },
+                                "&:hover fieldset": { borderColor: "gray" },
+                                "&.Mui-focused fieldset": { borderColor: "white" },
+                            },
+                            "& .MuiInputLabel-root": { color: "white" }, // Label color
+                            "& .MuiInputLabel-root.Mui-focused": { color: "white" },
+                            "& .MuiInputBase-input::placeholder": { color: "rgba(255, 255, 255, 0.7)" }, // Placeholder color
+                        }}
                     />
                 </section>
 
