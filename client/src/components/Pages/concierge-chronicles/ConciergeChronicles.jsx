f314f1fcb3e829685196c555ffc127f1ba0b9291
JaQwae <jaqwaeellison@gmail.com>
2025-03-26 23:04:25 -0700

    the filter buttons are working for both the main 4 and the modal container, correct articles are shown based on what the user clickes

--- client/src/components/Pages/concierge-chronicles/ConciergeChronicles.jsx
+++ client/src/components/Pages/concierge-chronicles/ConciergeChronicles.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ChroniclesData from './ChroniclesData';
-import './ConciergeChronicles.css'
+import Buttons from '../../Buttons/Button'
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import './ConciergeChronicles.css';
 
 const ConciergeChronicles = ({ navHeight }) => {
+    const [selectedCategory, setSelectedCategory] = useState('All');
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    // Extract unique categories for filtering options
+    const allCategories = ['All', ...[...new Set(ChroniclesData.map(article => article.category))].sort()];
+
+
+    // Main 4 buttons (adjust these to fit your needs)
+    const mainCategories = ['All', 'TIPS', 'JOURNEYS', 'CULTURE'];
+
+    // Filter articles based on selected category
+    const filteredArticles = selectedCategory === 'All'
+        ? ChroniclesData
+        : ChroniclesData.filter(article => article.category === selectedCategory);
+
     return (
-        <div
-            style={{ marginTop: `${navHeight}px` }}
-            className='pages'
-        >
-            <h1>ConciergeChronicles</h1>
+        <div style={{ marginTop: `${navHeight}px` }} className='pages'>
+            <h1>Concierge Chronicles</h1>
+
+            {/* Filter Buttons */}
+            <div className="filter-buttons">
+                <Buttons displayName={<FilterAltIcon />} btnAction={handleOpen}>Open modal</Buttons>
+                {mainCategories.map(category => (
+                    <Buttons
+                        key={category}
+                        displayName={category}
+                        btnClassName={selectedCategory === category ? 'active' : ''}
+                        btnAction={() => setSelectedCategory(category)}
+                    />
+                ))}
+            </div>
+
+            {/* Display Filtered Articles */}
             <div className="chronicles-container">
-                {ChroniclesData.map((article, index) => (
-                    <div className="chronicle-card" key={index}>
+                {filteredArticles.map((article) => (
+                    <div className="chronicle-card" key={article.title}>
                         <img src={article.image} alt={article.alt} className="chronicle-image" />
                         <div className="chronicle-content">
                             <h3 className="chronicle-title">{article.title}</h3>
@@ -24,12 +58,36 @@ const ConciergeChronicles = ({ navHeight }) => {
                     </div>
                 ))}
             </div>
-        </div>
-    )
-}
 
-export default ConciergeChronicles
+            {/* Filter Btn Modal */}
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box className="modal-box">
+                    <div className="modal-categories">
+                        {allCategories.map(category => (
+                            <Buttons
+                                key={category}
+                                displayName={category}
+                                btnClassName={selectedCategory === category ? 'active' : ''}
+                                btnAction={() => {
+                                    setSelectedCategory(category);
+                                    handleClose();
+                                }}
+                            />
+                        ))}
+                    </div>
+                </Box>
+            </Modal>
+        </div>
+    );
+};
+
+export default ConciergeChronicles;
 
 ConciergeChronicles.propTypes = {
     navHeight: PropTypes.number
-}
+};
