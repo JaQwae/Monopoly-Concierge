e3bb935f93850f170a43706c0ee8ef8edb247653
JaQwae <jaqwaeellison@gmail.com>
2025-04-03 01:40:11 -0700

    Jet charter wigdet skeleton created

--- client/src/components/Pages/jet-charters/JetCharters.jsx
+++ client/src/components/Pages/jet-charters/JetCharters.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-// import './Properties.css'
+// import planeIcon from '../../../assets/jet-charters/plane-icon.png';
+import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
+import FlightLandIcon from '@mui/icons-material/FlightLand';
+import PersonIcon from '@mui/icons-material/Person';
+import './JetCharters.css'
 
 import PropTypes from 'prop-types'
 
@@ -9,7 +13,26 @@ const JetCharters = ({ navHeight }) => {
       style={{ marginTop: `${navHeight}px` }}
       className='pages'
     >
-      <h1>Jets</h1>
+      <section id='charter-hero-container'>
+          <div id='jet-form-widget'>
+            <div className='widget-section'>
+              <FlightTakeoffIcon className='widget-image' style={{ fontSize: '2.5em' }}/>
+              {/* <img className='widget-image' src={planeIcon} alt='airplane icon'/> */}
+              <input className='widget-input'></input>
+            </div>
+            <div className='widget-section'>
+              <FlightLandIcon className='widget-image' style={{ fontSize: '2.5em' }}/>
+              {/* <img className='widget-image' src={planeIcon} alt='airplane icon'/> */}
+              <input className='widget-input'></input>
+            </div>
+            <div className='widget-section'>
+              <PersonIcon className='widget-image' style={{ fontSize: '2.5em' }}/>
+              {/* <img className='widget-image' src={planeIcon} alt='airplane icon'/> */}
+              <input className='widget-input'></input>
+            </div>
+            <button>Update</button>
+          </div>
+      </section>
     </div>
   )
 }
