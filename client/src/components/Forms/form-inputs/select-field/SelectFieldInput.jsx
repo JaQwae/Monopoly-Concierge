5deac0115ab417e1973a35fb6c6703e477708f06
JaQwae <jaqwaeellison@gmail.com>
2025-04-14 15:37:17 -0700

    fixed styling bugs with dropdown field input

--- client/src/components/Forms/form-inputs/select-field/SelectFieldInput.jsx
+++ client/src/components/Forms/form-inputs/select-field/SelectFieldInput.jsx
@@ -1,38 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
-import './SelectFieldInput.css'
+import './SelectFieldInput.css';
 
-const SelectFieldInput = ({ label, value, onChange, options, className}) => {
+const SelectFieldInput = ({ label, value, onChange, options, className }) => {
     return (
-        <FormControl fullWidth={true} margin='normal' className={className}>
-            <InputLabel
-                sx={{
-                    color: 'var(--primary-color)', //Makes label text white before clicking
-                    "&.Mui-focused": { color: 'var(--primary-color)' },
-                }}
-            >
+        <FormControl fullWidth margin='normal' className={`dropdown-input-container ${className || ''}`}>
+            <InputLabel className="dropdown-input-title">
                 {label}
             </InputLabel>
             <Select
                 value={value}
                 onChange={onChange}
                 label={label}
-                sx={{
-                    color: "var(--primary-color)", // Makes selected text white
-                }}
+                className="dropdown-input-select"
                 MenuProps={{
                     PaperProps: {
-                        sx: {
-                            "& .MuiList-root": {
-                                padding: 0, // Removes padding from the dropdown menu
-                            },
-                        },
+                        className: 'dropdown-menu-container',
                     },
                 }}
             >
                 {options.map((option) => (
-                    <MenuItem key={option.value} value={option.value} className="dropdown-options">
+                    <MenuItem 
+                        key={option.value} 
+                        value={option.value} 
+                        className="dropdown-menu-options"
+                    >
                         {option.label}
                     </MenuItem>
                 ))}
@@ -48,12 +41,10 @@ SelectFieldInput.propTypes = {
     options: PropTypes.arrayOf(
         PropTypes.shape({
             value: PropTypes.string.isRequired,
-            label: PropTypes.string.isRequired
+            label: PropTypes.string.isRequired,
         })
     ).isRequired,
     className: PropTypes.string,
 };
-
-
 
 export default SelectFieldInput;
