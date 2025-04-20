8d07351e857741ed89816fadb1d3d8413b3786bb
JaQwae <jaqwaeellison@gmail.com>
2025-04-20 10:04:40 -0700

    updated propertry links

--- client/src/data/propertyData.js
+++ client/src/data/propertyData.js
@@ -8,7 +8,7 @@ const propertyData = [
         altTxt: ' Luxury outdoor massage with hot tub at The Isabella Oasis, a Monopoly Concierge property in Houston',
         text: 'Your private urban escape near Downtown Houston. This spacious 4-bed, 4-bath duplex offers serenity and luxury just minutes from Midtown and popular attractions. With room for up to 14 guests, it’s perfect for intimate getaways or celebrations—complete with dual kitchens, living areas, and parking for seven.',
         boldText: 'Reserve your stay today and discover the perfect blend of space and style in the heart of Houston.',
-        btnAction: 'https://www.isabellaoasis.com/'
+        btnAction: 'https://www.isabellaoasis.com/listings/185395'
     },
     {
         title: 'House of Tunes',
@@ -16,7 +16,7 @@ const propertyData = [
         altTxt: 'Music-themed corner at House of Tunes, a Monopoly Concierge property, featuring a vinyl side table, golden lamp lighting, and a repurposed piano frame art piece',
         text: 'Escape to a fully gated retreat just outside Houston Heights, where rhythm meets relaxation. Enjoy snacks and wine at the Vinyl Bar, cook in the spacious kitchen, or unwind in the hot tub beneath murals celebrating icons like ZZ Top and Selena. Unique touches like guitar pick coffee tables and the bold Beyoncé Room make every corner unforgettable.',
         boldText: ' Reserve your stay today and experience the perfect harmony of style and comfort.',
-        btnAction: 'https://www.houseoftuneshtx.com/'
+        btnAction: 'https://www.houseoftuneshtx.com/listings/301794'
     }
 ]
 
