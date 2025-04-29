12be653c414dc2b8d33ff2e7466ecdcebcce8693
JaQwae <jaqwaeellison@gmail.com>
2025-04-28 19:09:33 -0700

    added a few cars that were missing and updated prices

--- client/src/data/CarRentalsData.js
+++ client/src/data/CarRentalsData.js
@@ -1,42 +1,49 @@
-// import {
-//     // bentleyBentayga,
-//     // chevroletC8Corvette,
-//     // cadillacEscalade,
-//     // rollsRoyceWraith,
-//     // rollsRoyceCullinan,
-//     // mercedesBenzSClass,
-//     // mercedesBenzGWagonAMG,
-//     // mercedesBenzMaybachSedan
-// } from '../assets/car-rentals/carRentalImage.js'
-
 const CarRentalsData = [
     {
         title: "Bentley Bentayga",
-        price: "$ 0,000",
+        price: "$ 1,100",
         imageUrl: 'https://www.europeanprestige.co.uk/blobs/stock/376/images/c87aa1b8-1862-4ede-834d-b709978eef09/hi4a4511.jpg?width=2000&height=1333',
         category: 'Luxury Rentals'
     },
     {
+        title: "Bentley Continental GT",
+        price: "$ 1,000",
+        imageUrl: 'https://media.ed.edmunds-media.com/bentley/continental/2025/oem/2025_bentley_continental_coupe_gt-black-edition_fq_oem_1_600.jpg',
+        category: 'Luxury Rentals'
+    },
+    {
         title: "Chevrolet C8 Corvette",
-        price: "$ 0,000",
+        price: "$ 400",
         imageUrl: "https://hips.hearstapps.com/hmg-prod/images/2023-chevrolet-corvette-stingray-convertible-3lt-z51-307-1665496970.jpg?crop=0.635xw:0.535xh;0.0897xw,0.417xh&resize=2048:*",
         category: 'Luxury Rentals'
     },
     {
+        title: "Mclaren 720s",
+        price: "$ 1,200",
+        imageUrl: "https://laexoticcarrentalsandsales.com/cdn/shop/products/image_f6ef7432-fb9d-4f58-bf52-cc4c8799946d.jpg?v=1675237601&width=1946",
+        category: 'Luxury Rentals'
+    },
+    {
         title: "Cadillac Escalade",
-        price: "$ 0,000",
+        price: "$ 400",
         imageUrl: "https://di-uploads-pod46.dealerinspire.com/cadillacofcalabasas/uploads/2025/02/25Cadillac-Escalade-ExteriorInDriveway-21x9-1.jpg",
+        category: 'Luxury Rentals'
+    },
+    {
+        title: "Rolls-Royce Phantom",
+        price: "$ 1,300",
+        imageUrl: "https://cimg3.ibsrv.net/ibimg/hgm/1920x1080-1/100/954/2025-rolls-royce-phantom-dragon--china_100954503.jpg",
         category: 'Chauffeured Rentals'
     },
     {
         title: "Rolls Royce Wraith",
-        price: "$ 0,000",
+        price: "$ 1,200",
         imageUrl: "https://cdn.motor1.com/images/mgl/NkxNG/s1/rolls-royce-wraith-black-badge-feature.jpg",
-        category: 'Chauffeured Rentals'
+        category: 'Luxury Rentals'
     },
     {
         title: "Rolls Royce Cullinan",
-        price: "$ 0,000",
+        price: "$ 1,400",
         imageUrl: "https://www.europeanprestige.co.uk/blobs/stock/485/images/e4fb48fd-808b-4b51-857d-e7a8d9a1db96/hi4a3448.jpg?width=2000&height=1333",
         category: 'Chauffeured Rentals'
     },
@@ -47,44 +54,62 @@ const CarRentalsData = [
         category: 'Luxury Rentals'
     },
     {
-        title: "Mercedes-Benz G-Wagon AMG",
-        price: "$ 0,000",
+        title: "Mercedes Benz AMG G 63",
+        price: "$ 800",
         imageUrl: "https://www.motortrend.com/uploads/2022/02/2022-Mercedes-Benz-G-Class-AMG-G63-22.jpg",
         category: 'Luxury Rentals'
     },
     {
         title: "Mercedes Benz Maybach Sedan",
-        price: "$ 0,000",
+        price: "$ 800",
         imageUrl: "https://images.dealersync.com/2981/Photos/1120930/20240406174858347_RMB_9982.jpg?_=7358f3c68b89c5ec74354e149a899d5fe1c06d81" ,
-        category: 'Luxury Rentals'
+        category: 'Chauffeured Rentals'
+    },
+    {
+        title: "Mercedes Benz Maybach SUV",
+        price: "$ 900",
+        imageUrl: "https://i.ytimg.com/vi/AFtDCAwfyzo/maxresdefault.jpg" ,
+        category: 'Chauffeured Rentals'
     },
     {
         title: "Mercedes Sprinter Van",
-        price: "$ 0,000",
+        price: "$ 1,200",
         imageUrl: "https://www.pcarmarket.com/static/media/uploads/galleries/photos/uploads/galleries/19261-sydney-black-sprinter-van/.thumbnails/SprinterVan-29.jpg/SprinterVan-29-tiny-2048x0.jpg" ,
         category: 'Chauffeured Rentals'
     },
     {
-        title: "Lamborghini Huracan Coupe",
-        price: "$ 0,000",
+        title: "Lamborghini Huracan",
+        price: "$ 1,400",
         imageUrl: "https://imagescdn.dealercarsearch.com/Media/22602/21778716/638640180355744068.jpg?auto_fix=false&quality=100&sharp_amount=false",
         category: 'Luxury Rentals'
     },
     {
         title: "Lamborghini Huracan Convertible",
-        price: "$ 0,000",
+        price: "$ 1,600",
         imageUrl: "https://suntimeexoticcarrentalsatlanta.com/wp-content/uploads/2022/08/Lamborghini-Huracan-Spyder-Orange-1.jpg" ,
         category: 'Luxury Rentals'
     },
     {
+        title: "Lamborghini Urus",
+        price: "$ 1,200",
+        imageUrl: "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2023%2F06%2Flamborghini-urus-s-closer-look-photos-review-0.jpg?w=960&cbr=1&q=90&fit=max",
+        category: 'Luxury Rentals'
+    },
+    {
+        title: "Lamborghini Aventador",
+        price: "$ 1,800",
+        imageUrl: "https://images.unsplash.com/photo-1621135802920-133df287f89c?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFtYm9yZ2hpbmklMjBhdmVudGFkb3J8ZW58MHx8MHx8fDA%3D" ,
+        category: 'Luxury Rentals'
+    },
+    {
         title: "Lamborghini Aventador Convertible",
-        price: "$ 0,000",
+        price: "$ 2,000",
         imageUrl: "https://www.pcarmarket.com/static/media/uploads/galleries/photos/uploads/galleries/40317-seale-2015-lamborghini-aventador-roadster/.thumbnails/IMG_0778_RwuB6iG_lqtC2gJ.webp/IMG_0778_RwuB6iG_lqtC2gJ-tiny-1200x0.webp" ,
         category: 'Luxury Rentals'
     },
     {
         title: "Ferrari 488",
-        price: "$ 0,000",
+        price: "$ 1,600",
         imageUrl: "https://pbs.twimg.com/media/DTRATFfW4AANxqF.jpg:large" ,
         category: 'Luxury Rentals'
     },
@@ -95,14 +120,8 @@ const CarRentalsData = [
         category: 'Luxury Rentals'
     },
     {
-        title: "Lamborghini Urus",
-        price: "$ 900",
-        imageUrl: "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2023%2F06%2Flamborghini-urus-s-closer-look-photos-review-0.jpg?w=960&cbr=1&q=90&fit=max",
-        category: 'Luxury Rentals'
-    },
-    {
         title: "Porsche 911 Turbo",
-        price: "$ 1,000",
+        price: "$ 800",
         imageUrl: "https://images.unsplash.com/photo-1698131789135-9328c3e5644a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fFBvcnNjaGUlMjA5MTElMjBUdXJibyUyMFN8ZW58MHx8MHx8fDA%3D",
         category: 'Luxury Rentals'
     },
