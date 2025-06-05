b88b64ce257732310bb3937ced432435d2cd7816
JaQwae <jaqwaeellison@gmail.com>
2025-06-05 01:26:44 -0700

    updated chronicle photo

--- client/src/assets/chronicles/chronicles.mjs
+++ client/src/assets/chronicles/chronicles.mjs
@@ -5,7 +5,7 @@ import whyPrivateJetTravelIsWorthIT from './whyPrivateJetTravelIsWorthIT.jpg';
 import theRealCostOfFlyingPrivate from './theRealCostOfFlyingPrivate.jpg';
 import theFlightYouDeserve from './theFlightYouDeserve.jpg';
 import fuelArbitrage from './fuelArbitrage.jpg';
-import howToMakeYourNextStayUnforgettableWithConciergeServices from './howToMakeYourNextStayUnforgettableWithConciergeServices.jpg';
+import howToMakeYourNextStayUnforgettableWithConciergeServices from './howToMakeYourNextStayUnforgettableWithConciergeServices .jpg';
 import vehicleTravelRedefined from './vehicleTravelRedefined.jpg';
 import theDifferenceBetweenBrokersAndOperatorsInPrivateJetTravel from './theDifferenceBetweenBrokersAndOperatorsInPrivateJetTravel.jpg'
 
